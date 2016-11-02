2a0899f7c909bde786bb3a975e758bc194ce9dec
Lokesh Devnani <contact@lokeshd.com>
2016-11-02 19:54:17 +0530

    Splitting scripts done

--- splitipy/splitipy.py
+++ splitipy/splitipy.py
@@ -3,16 +3,19 @@ splitipy
 
 Usage:
   splitipy <file>
+  splitipy <file> [--size=<mb>]
   splitipy -j <file> | --join <file>
   splitipy -h | --help
   splitipy --version
 
 Options:
   -h --help                         Show this screen.
+  --size=<mb>                       File size of each splitted file [default: 10].
   --version                         Show version.
 
 Examples:
   splitipy hello
+  splitipu hello.mp4 --size 2000
 
 Help:
   For help using this tool, please open an issue on the Github repository:
@@ -31,14 +34,13 @@ def main():
     options = docopt(__doc__, version=__version__)
     print(options)
     if (options['-j'] or options['--join']):
-        print "yay joining "+ options['<file>']
+        Stuff.combine(options['<file>'], options)
     elif(options['<file>']):
-        print options['<file>']
-    
+        Stuff.split(options['<file>'], options['--size'])
 
-    print("Executing bootstrap version %s." % __version__)
-    print("List of argument strings: %s" % sys.argv[1:])
-    print("Stuff and Boo():\n%s\n%s" % (Stuff, Boo()))
+    # print("Executing bootstrap version %s." % __version__)
+    # print("List of argument strings: %s" % sys.argv[1:])
+    # print("Stuff and Boo():\n%s\n%s" % (Stuff, Boo()))
 
 
 class Boo(Stuff):
--- splitipy/stuff.py
+++ splitipy/stuff.py
@@ -1,8 +1,47 @@
 # -*- coding: utf-8 -*-
 
-
 """splitipy.stuff: stuff module within the splitipy package."""
+from __future__ import division
+from math import ceil
+import os
+
+class Stuff():
+
+	@staticmethod
+	def split(sfile, splitsize):
+
+		if os.path.isfile(sfile):
+			filesize = os.stat(sfile).st_size
+		else:
+			print "Hey, the file named "+sfile+" doesn't exists."
+			return 1
+
+		splitsize = int(splitsize) * 1024 * 1024 #converting into MBs
+		bsize=1024 * 1024
+		if bsize > splitsize:
+			bsize = splitsize
+
+		times = int(ceil(splitsize/bsize))
+		parts = int(ceil(filesize/splitsize))
+
+		print "Splitting file "+ str(sfile) + " into "+ str(parts) + " parts";
+		try:
+			i=1
+			block=True
+			fcombine  = open(sfile,"rb")
+			while block!="" and i<=parts:
+				fw = open(sfile+"."+str(i),"wb")
+				j=1
+				while block!="" and j<=times:
+					block = fcombine.read(bsize)
+					fw.write(block)
+					j+=1
+				fw.close()
+				i+=1
+		finally:
+			fcombine.close()
 
 
-class Stuff(object):
-    pass
+	@staticmethod
+	def combine(jfile, options):
+		print( "joining " + jfile)
