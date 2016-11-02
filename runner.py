280542d566ef14615a8fca3a2448fe0b42dd0fcb
Lokesh Devnani <contact@lokeshd.com>
2016-11-02 15:59:42 +0530

    script runner added for quick invocation

---
+++ runner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""Convenience wrapper for running splitipy directly from source tree."""
+
+
+from splitipy.splitipy import main
+
+
+if __name__ == '__main__':
+    main()
